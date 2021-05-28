c955b1612689279b60863d8300d9351f138f3e85
Samo <samo.metlicar@hotmail.com>
2021-05-28 23:16:26 +0200

    SGTC
    https://www.vldb.org/pvldb/vol6/p1870-aduri.pdf

---
+++ src/main/java/triangle_counting/Estimator.java
@@ -0,0 +1,91 @@
+package triangle_counting;
+
+import java.util.ArrayList;
+
+public class Estimator {
+    int[] r1;
+    int[] r2;
+    int[] t;
+    int c, betax, betay, r2pos;
+    public Estimator() {
+        r1 = null;
+        r2 = null;
+        t = null;
+        c = 0;
+        betax = 0;
+        betay = 0;
+        r2pos = -1;
+    }
+
+    public void setLevelOneEdge(int[] e) {
+        r1 = e;
+        c = 0;
+        r2 = null;
+        t = null;
+        betax = 0;
+        betay = 0;
+    }
+
+    public void setLevelTwoEdge(int[] e, int i) {
+        r2 = e;
+        r2pos = i;
+        t = null;
+    }
+
+    public void setBeta(int x, int y) {
+        betax = x;
+        betay = y;
+    }
+
+    public ArrayList<Integer> getMissingEdge() {
+        // Assuming edges are ordered in increasing order (ri[0] < ri[1]) and are unique
+        // int[] e = new int[2];
+        ArrayList<Integer> e = new ArrayList<>();
+        if (r2 == null) return e;
+        if (r1[0] == r2[0]) {
+            if (r1[1] < r2[1]) {
+                e.add(r1[1]);
+                e.add(r2[1]);
+            } else {
+                e.add(r2[1]);
+                e.add(r1[1]);
+            }
+        } else if (r1[0] == r2[1]) {
+            e.add(r2[0]);
+            e.add(r1[1]);
+        } else if (r1[1] == r2[0]) {
+            e.add(r1[0]);
+            e.add(r2[1]);
+        } else if (r1[1] == r2[1]){
+            if (r1[0] < r2[0]) {
+                e.add(r1[0]);
+                e.add(r2[0]);
+            } else {
+                e.add(r2[0]);
+                e.add(r1[0]);
+            }
+        } else {} // r2 does not match with r1, because there are no matching edges in the stream after r1
+        return e;
+    }
+
+    public void print() {
+        String[] output = new String[13];
+        output[0] = "r1=(";
+        output[1] = String.valueOf(r1[0]);
+        output[2] = ",";
+        output[3] = String.valueOf(r1[1]);
+        output[4] = "), r2=(";
+        output[5] = String.valueOf(r2 == null ? -1 : r2[0]);
+        output[6] = ",";
+        output[7] = String.valueOf(r2 == null ? -1 : r2[1]);
+        output[8] = "), t=(";
+        output[9] = String.valueOf(t == null ? -1 : t[0]);
+        output[10] = ",";
+        output[11] = String.valueOf(t == null ? -1 : t[1]);
+        output[12] = ")";
+        for (int i = 0; i < output.length; i++) {
+            System.out.print(output[i]);
+        }
+        System.out.println();
+    }
+}
---
+++ src/main/java/triangle_counting/StreamGraphTriangleCounter.java
@@ -0,0 +1,172 @@
+package triangle_counting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
+
+import static java.lang.Math.round;
+
+public class StreamGraphTriangleCounter {
+    Estimator[] estimators;
+    Random random;
+    int m; // Number of edges already checked
+    HashMap<Integer, ArrayList<Estimator>> L;
+    HashMap<ArrayList<Integer>, ArrayList<Estimator>> P;
+    HashMap<ArrayList<Integer>, ArrayList<Estimator>> Q;
+
+    public StreamGraphTriangleCounter(int r) {
+        estimators = new Estimator[r];
+        for (int i = 0; i < r; i++) {
+            estimators[i] = new Estimator();
+        }
+        random = new Random();
+        m = 0;
+    }
+
+    /**
+     * https://www.vldb.org/pvldb/vol6/p1870-aduri.pdf
+     * @param B stream of edges
+     */
+    public void bulkTC(int[][] B) {
+        int randInt, idx;
+        L = new HashMap<>(); // Part of Step 2
+
+        // Step 1
+        for (Estimator est: estimators) {
+            randInt = random.nextInt(m + B.length);
+            if (randInt >= m) {
+                idx = randInt - m;
+                est.setLevelOneEdge(B[idx]);
+
+                // Part of Step 2
+                L.computeIfAbsent(idx, k -> new ArrayList<>());
+                L.get(idx).add(est);
+            }
+        }
+
+        // Step 2
+        P = new HashMap<>();
+        HashMap<Integer, Integer> deg = edgeIterA(B);
+
+        for (Estimator est: estimators) {
+            randEdgeIntoEvent(est, deg);
+        }
+
+        edgeIterB(B);
+
+        // Step 3
+        // Construct Q
+        ArrayList<Integer> e;
+        Q = new HashMap<>();
+        ArrayList<Estimator> Qi;
+        for (Estimator est: estimators) {
+            e = est.getMissingEdge();
+            Q.computeIfAbsent(e, k -> new ArrayList<>());
+            Q.get(e).add(est);
+        }
+
+        // Find triangles
+        for (int i = 0; i < B.length; i++) {
+            Qi = Q.get(Utils.arrayToList(B[i]));
+            if (Qi != null) {
+                for (Estimator est: Qi) {
+                    if (i > est.r2pos) {
+                        est.t = B[i];
+                    }
+                }
+            }
+        }
+
+        m += B.length;
+    }
+
+    private HashMap<Integer, Integer> edgeIterA(int[][] B) {
+        HashMap<Integer, Integer> deg = new HashMap<>();
+        int x, y, degx, degy;
+        for (int i = 0; i < B.length; i++) {
+            if (B[i] == null) {
+                System.out.println("WTF???");
+            }
+            x = B[i][0];
+            y = B[i][1];
+            degx = deg.getOrDefault(x, 0);
+            degy = deg.getOrDefault(y, 0);
+            deg.put(x, ++degx);
+            deg.put(y, ++degy);
+            eventA(i, x, y, deg);
+        }
+        return deg;
+    }
+
+    private void edgeIterB(int[][] B) {
+        HashMap<Integer, Integer> deg = new HashMap<>();
+        int x, y, degx, degy;
+        for (int i = 0; i < B.length; i++) {
+            x = B[i][0];
+            y = B[i][1];
+            degx = deg.getOrDefault(x, 0);
+            degy = deg.getOrDefault(y, 0);
+            deg.put(x, ++degx);
+            deg.put(y, ++degy);
+            eventB(i, B[i], x, degx);
+            eventB(i, B[i], y, degy);
+        }
+    }
+
+    private void eventA(int i, int x, int y, HashMap<Integer, Integer> deg) {
+        ArrayList<Estimator> Li = L.get(i);
+        if (Li != null) {
+            for (Estimator est: L.get(i)) {
+                est.setBeta(deg.getOrDefault(x, 0), deg.getOrDefault(y, 0));
+            }
+        }
+    }
+
+    private void eventB(int i, int[] e, int v, int a) {
+        ArrayList<Integer> key = new ArrayList<>(Arrays.asList(v, a));
+        ArrayList<Estimator> Pi = P.get(key);
+        if (Pi != null) {
+            for (Estimator est: Pi) {
+                est.setLevelTwoEdge(e, i);
+            }
+        }
+    }
+
+    private void randEdgeIntoEvent(Estimator est, HashMap<Integer, Integer> deg) {
+        int a = deg.getOrDefault(est.r1[0], 0);
+        a -= est.betax;
+        int b = deg.getOrDefault(est.r1[1], 0);
+        b -= est.betay;
+        int cminus = est.c;
+        int cplus = a+b;
+
+        // Update values
+        est.c += cplus;
+        est.setBeta(0, 0);
+        if (cminus+cplus == 0) {
+            return;
+        }
+        int phi = random.nextInt(cminus+cplus);
+        ArrayList<Integer> i;
+        if (phi < cminus) {
+            // Keep existing r2
+            return;
+        } else if (phi < cminus + a) {
+            i = new ArrayList<>(Arrays.asList(est.r1[0], est.betax + phi - cminus));
+        } else {
+            i = new ArrayList<>(Arrays.asList(est.r1[1], est.betay + phi - cminus - a));
+        }
+        P.computeIfAbsent(i, k -> new ArrayList<>());
+        P.get(i).add(est);
+    }
+
+    public long estimateTriangles() {
+        float sum = 0;
+        for (Estimator est: estimators) {
+            if (est.t != null) sum += est.c * m;
+        }
+
+        return round(sum / estimators.length);
+    }
+}
